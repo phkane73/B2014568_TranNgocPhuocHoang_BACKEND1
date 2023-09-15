b68a09d99b9b801e2dbb88119ced17fee7bc7fdd
phkane73 <phkane732002@gmail.com>
2023-09-15 23:10:40 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const contactRoutes = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +10,13 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 app.use("/api/contacts", contactRoutes);
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found."));
+});
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -1,19 +1,20 @@
-const express = require('express');
-const contacts = require('../controller/contact.controller');
+const express = require("express");
+const contacts = require("../controller/contact.controller");
 
 const router = express.Router();
 
-router.route("/")
-    .get(contacts.findAll)
-    .post(contacts.create)
-    .delete(contacts.deleteAll);
+router
+  .route("/")
+  .get(contacts.findAll)
+  .post(contacts.create)
+  .delete(contacts.deleteAll);
 
-router.route("/favorite")
-    .get(contacts.findAllFavorite);
+router.route("/favorite").get(contacts.findAllFavorite);
 
-router.route("/:id")
-    .get(contacts.findOne)
-    .put(contacts.update)
-    .delete(contacts.delete);
+router
+  .route("/:id")
+  .get(contacts.findOne)
+  .put(contacts.update)
+  .delete(contacts.delete);
 
 module.exports = router;

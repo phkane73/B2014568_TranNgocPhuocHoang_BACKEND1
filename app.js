bae18632769eb79b4caa6c525fefccce8e701af5
phkane73 <phkane732002@gmail.com>
2023-09-15 22:56:31 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactRoutes = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
+app.use("/api/contacts", contactRoutes);
 
 module.exports = app;
